a8102b32eb6dcbf4eb1a9748db893431a0388391
dns <dynuli01@mail.com>
2024-12-19 14:30:57 +0700

    Добавление классов делегатов

--- Лабораторная №4/WorkInterface.h
+++ Лабораторная №4/WorkInterface.h
@@ -11,3 +11,47 @@ class Testing {
 public:
     virtual void spendTesting() = 0; // Метод для проведения тестирования
 };
+
+//Классы делегатов
+class MainManager : public ProjectManagement { // класс поведения для работников, которые руководят проектом
+public:
+    void manageProject() {
+        printf("Я управляю разработчиками!\n");
+    }
+};
+
+class AssistentManager : public ProjectManagement { // класс поведения для работников, которые управля проектом
+public:
+    void manageProject() {
+        printf("Я управляю проектом!\n");
+    }
+};
+
+class NoManager : public ProjectManagement { // класс поведения для работников, которые управля проектом
+public:
+    void manageProject() {
+        printf("Я не управляю проектом!\n");
+    }
+};
+
+class MainTester : public Testing { // класс поведения для работников, которые тестируют программу
+public:
+    void spendTesting() {
+        printf("Я занимаюсь тестировкой готового проекта!\n");
+    }
+};
+
+class AssistentTester : public Testing { // класс поведения для работников, которые тестируют программу
+public:
+    void spendTesting() {
+        printf("Я занимаюсь первичной тестировкой проекта, на этапе реализации!\n");
+    }
+};
+
+class NoTester : public Testing { // класс поведения для работников, которые тестируют программу
+public:
+    void spendTesting() {
+        printf("Я не занимаюсь тестировкой проекта!\n");
+    }
+};
+
